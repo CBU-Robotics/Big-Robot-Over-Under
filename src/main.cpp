d78048e7cf4078ad7a439b64f3e90f2f447a566f
MichaelPendleton <michaelcurtis.pendleton@calbaptist.edu>
2024-03-01 10:15:29 -0800

    Removed unecessary bool

--- src/main.cpp
+++ src/main.cpp
@@ -59,14 +59,12 @@ void autonomous() {
 double dabs(double v) { return v < 0 ? -v : v; }
 
 void opcontrol() {
-  bool firstLaunch = true;
   while (true) {
     // Launch and reload the catapult
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
       catapult.move_relative(-723, 100);
       pros::delay(250);
       punch();
-
     // if the joystick is not at 0, move the catapult manuelly to fix cam issues
     } else if (master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) != 0){
       const double MAX_RPM = 30.;
@@ -85,7 +83,6 @@ void opcontrol() {
       pros::delay(1000);
       puncher_motor.brake();
     }
-
     pros::delay(20);
   }
 }

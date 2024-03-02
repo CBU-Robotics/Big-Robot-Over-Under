0d40ff71c15726aefa092e06881021b59db1296a
eliasmurcray <eliasmurcray@gmail.com>
2024-03-02 09:49:03 -0800

    comp update 1

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 const int LEFT_CAT_MOTOR_PORT = 11;
 const int RIGHT_CAT_MOTOR_PORT = 2;
 const int Puncher_Motor_Port = 10;
+const int SPIN_MOTOR_PORT = 12;
 
 int count = 0;
 
@@ -13,9 +14,12 @@ pros::ADIDigitalIn limit('A');
 pros::Motor left_cat_motor(LEFT_CAT_MOTOR_PORT, pros::E_MOTOR_GEAR_GREEN, false,
                            pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor right_cat_motor(RIGHT_CAT_MOTOR_PORT, pros::E_MOTOR_GEAR_GREEN,
-                              true, pros::E_MOTOR_ENCODER_DEGREES);
+                            true, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor puncher_motor(Puncher_Motor_Port, pros::E_MOTOR_GEAR_RED, true,
                           pros::E_MOTOR_ENCODER_DEGREES);
+
+pros::Motor spin_motor(SPIN_MOTOR_PORT, pros::E_MOTOR_GEAR_RED, true,
+                       pros::E_MOTOR_ENCODER_DEGREES);
 
 pros::Motor_Group catapult({left_cat_motor, right_cat_motor});
 
@@ -39,19 +43,20 @@ void disabled() {}
 
 void competition_initialize() {}
 
-// We have one preload, one alliance triball, and 10 match loads that can be introduced in autonomous
-// Small Robot also has one preload
+// We have one preload, one alliance triball, and 10 match loads that can be
+// introduced in autonomous Small Robot also has one preload
 void autonomous() {
   pros::delay(5000);
   // Run the catapult once which is 723 degrees to lauch preload
-  catapult.move_relative(-723, 100);
+  catapult.move_relative(-726, 100);
   puncher_motor.move_relative(-100, 70);
   pros::delay(500); // 250ms not enough time.
   // Then loop to load and launch catapult 11 times.
-  for (int i = 0; i < 11; i++) {  // 11 for normal matches
-    punch(); // 2 seconds
-    catapult.move_relative(-723, 100);
-    pros::delay(250); // reduced from 500ms to 250ms to load while catpult is resetting
+  for (int i = 0; i < 11; i++) { // 11 for normal matches
+    punch();                     // 2 seconds
+    catapult.move_relative(-726, 100);
+    pros::delay(
+        250); // reduced from 500ms to 250ms to load while catpult is resetting
   }
   puncher_motor.move_relative(100, 70);
 }
@@ -59,14 +64,16 @@ void autonomous() {
 double dabs(double v) { return v < 0 ? -v : v; }
 
 void opcontrol() {
+  spin_motor.move_relative(300, 20);
   while (true) {
     // Launch and reload the catapult
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
-      catapult.move_relative(-723, 100);
+      catapult.move_relative(-726, 100);
       pros::delay(250);
       punch();
-    // if the joystick is not at 0, move the catapult manuelly to fix cam issues
-    } else if (master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) != 0){
+      // if the joystick is not at 0, move the catapult manuelly to fix cam
+      // issues
+    } else if (master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y) != 0) {
       const double MAX_RPM = 30.;
       int t = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
       if (t < -10 || t > 10)
@@ -77,12 +84,11 @@ void opcontrol() {
     /**
      * IMPORTANT: Drop the intake to get match load introduced
      * between autonomous and driver control
-    */
+     */
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
-      puncher_motor.move_relative(-100, 70);
-      pros::delay(1000);
-      puncher_motor.brake();
+	punch();
     }
+
     pros::delay(20);
   }
 }
